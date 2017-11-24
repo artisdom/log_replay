7e39c7886e3eae9cb8333b5cefee135bc68fbcfc
Xigang Feng <xigang.feng@ericsson.com>
2017-11-24 18:35:39 +0800

    support config file to save current file stream offset.
    Change-Id: I88cdd83b22454f305b667951b82573a7535e0c07

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 all:
-	@rm -f log_replay
-	g++ -std=c++11 -pthread main.cpp -o log_replay
-
+	@rm -f log_replay test_boost
+	g++ -std=c++11 -pthread main.cpp -o log_replay -lboost_filesystem -lboost_system
+	g++ -std=c++11 test_boost.cpp -o test_boost -lboost_filesystem -lboost_system
+	g++ -std=c++11 test_boost3.cpp -o test_boost3 -lboost_filesystem -lboost_system
 install:
 	cp -v log_replay ~/bin/
--- main.cpp
+++ main.cpp
@@ -3,14 +3,18 @@
 #include <thread>
 #include <unistd.h>
 #include <termios.h>
+#include <sys/stat.h>
+#include <boost/filesystem.hpp>
 
 using namespace std;
+namespace fs = boost::filesystem;
 
 const int KEY_SPACE = ' ';
 const int KEY_F = 'f';
 const int KEY_B = 'b';
 const int KEY_J = 'j';
 const int KEY_N = 'n';
+const int KEY_Q = 'q';
 
 char getch() {
     char buf = 0;
@@ -40,6 +44,7 @@ char getch() {
 int ACTION_PAUSED = 0;
 int ACTION_SPEEDUP = 0;
 int ACTION_NEXTLINE = 0;
+int ACTION_QUIT = 0;
 
 //int ACTION_SLOWDOWN = 0;
 
@@ -67,24 +72,61 @@ int get_action()
             ACTION_SPEEDUP--;
             break;
         case KEY_J:
-            //printf("J key pressed.\n");
             ACTION_NEXTLINE++;
             break;
         case KEY_N:
-            //printf("N key pressed.\n");
             ACTION_NEXTLINE++;
+            break;
+        case KEY_Q:
+            ACTION_QUIT = 1;;
             break;
         }
     }
 }
 
+fs::path home_dir(getenv("HOME"));
+const string CONFIG_DIR_NAME = ".log_replay";
+fs::path config_dir = home_dir / CONFIG_DIR_NAME;
+
 int log_replay(char *filepath)
 {
-    ifstream filein(filepath);
+    fs::path p(filepath);
+    fs::path canon_p = canonical(p);
+
+    string curr_config_file_name;
+    ifstream filein(canon_p.string());
+    streampos file_offset = 0;
 
     if (!filein.is_open()) {
-        cout << "open file " << filepath << " failed.\n";
+        cout << "open file " << canon_p << " failed.\n";
         return -1;
+    }
+
+    for (const auto& dir : canon_p) {
+        if (dir == "/") {
+            continue;
+        }
+        curr_config_file_name += "_";
+        curr_config_file_name += dir.string();
+    }
+    //cout << "curr_config_file_name " << curr_config_file_name << "\n";
+
+    fs::path curr_config_file_path = config_dir / curr_config_file_name;
+
+    if (fs::exists(curr_config_file_path))
+    {
+        //std::cout << "config file exists: " << curr_config_file_path << std::endl;
+        ifstream curr_config_file(curr_config_file_path.string());
+        string file_offset_str;
+        std::getline(curr_config_file, file_offset_str);
+        file_offset = stoi(file_offset_str);
+
+        //cout << "file_offset_str: " << file_offset_str << "\n";
+        //cout << "file_offset: " << file_offset << "\n";
+
+        curr_config_file.close();
+
+        filein.seekg(file_offset, ios_base::beg);
     }
 
     for (string line; std::getline(filein, line); )
@@ -96,10 +138,18 @@ int log_replay(char *filepath)
                     cout << line << endl;
                     ACTION_NEXTLINE--;
                     break;
+                } else if (ACTION_QUIT) {
+                    ACTION_PAUSED = 0;
                 } else {
                     usleep(100);
                 }
             }
+        } else if (ACTION_QUIT) {
+            fstream f;
+            f.open(curr_config_file_path.string(), std::fstream::out | std::fstream::trunc);
+            f << filein.tellg();
+            f.close();
+            break;
         } else {
             usleep(300000);
             cout << line << endl;
@@ -116,12 +166,28 @@ void usage(char *argv[]) {
     return;
 }
 
+int create_config_dir(void)
+{
+    if (!(fs::exists(config_dir))) {
+        bool res = fs::create_directories(config_dir);
+        if (res) {
+            printf("create_directories succeed.\n");
+        } else {
+            printf("create_directories failed.\n");
+        }
+    } else {
+        //printf("config folder exists.\n");
+    }
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 2) {
         usage(argv);
         return -1;
     }
+
+    create_config_dir();
 
     std::thread t(log_replay, argv[1]);
 
